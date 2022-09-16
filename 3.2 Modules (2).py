48428b7be6c39f29691361dc4662468e15e5353d
Shastri11 <70877545+Shastri11@users.noreply.github.com>
2022-09-17 00:54:19 +0530

    Add files via upload

---
+++ 3.2 Modules (2).py
@@ -0,0 +1,22 @@
+
+import math
+import os
+
+from math import pi
+
+piValue  = 3.14
+
+print("piValue")
+print(piValue)
+
+# using math module
+print(math.pi)      #3.141592653589793
+print("Value of pi : ",math.pi)     #Value of pi :  3.141592653589793
+
+print(pi)
+print("C:\Development\Class\Python\CT9")
+
+# by using os module
+print("Current working directory : ",os.getcwd())      # Current working directory :  C:\Development\Class\Python\CT9
+
+
