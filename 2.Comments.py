0aac45e84da756d88ca74e762ac7097b8d43e7f3
Shastri11 <70877545+Shastri11@users.noreply.github.com>
2022-09-17 00:43:03 +0530

    Add files via upload

---
+++ 2.Comments.py
@@ -0,0 +1,34 @@
+# Owner : Credence Team          # Single Line comment
+# Company : Credence
+# Created On : 14-Apr-22
+# Modified on 15-Apr-22
+
+# ctrl + /   => comment/uncommnet 
+# ctrl + k + c  => comment
+# ctrl + k + u => uncomment
+
+
+# Multiline comments
+"""Owner : Credence Team          
+Company : Credence
+Created On : 14-Apr-22
+Modified on 15-Apr-22"""
+
+
+'''Owner : Credence Team          
+Company : Credence
+Created On : 14-Apr-22
+Modified on 15-Apr-22'''
+
+
+#print("Hello Credence")
+from datetime import MAXYEAR
+from unicodedata import name
+
+
+print(
+'''Owner : Credence Team          
+Company : Credence
+Created On : 14-Apr-22
+Modified on 15-Apr-22''')
+
