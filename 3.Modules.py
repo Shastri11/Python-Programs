4268359ed09a79ee5841840a94f9277111bb09a5
Shastri11 <70877545+Shastri11@users.noreply.github.com>
2022-11-05 04:18:00 +0530

    Add files via upload

---
+++ 3.Modules.py
@@ -0,0 +1,19 @@
+# import math
+import os
+from turtle import clear
+
+# piValue  = 3.14
+
+# print("piValue")
+# print(piValue)
+
+# # using math module
+# print(math.pi)      #3.141592653589793
+# print("Value of pi : ",math.pi)     #Value of pi :  3.141592653589793
+
+# print("C:\Development\Class\Python\CT9")
+
+# by using os module
+print("Current working directory : ",os.getcwd())      # Current working directory :  C:\Development\Class\Python\CT9
+
+print(clear)
